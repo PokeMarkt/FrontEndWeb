5ef0ce64bb460295e63785b7d2494de1dff3c3d9
Joan Torres <joan.torres.guimil@gmail.com>
2018-07-29 23:33:21 +0200

    Añadido login/registar

--- pokemarkt-frontend/src/components/Login.vue
+++ pokemarkt-frontend/src/components/Login.vue
@@ -7,23 +7,37 @@
                 <div class="modal-content">
 
                     <div class="modal-header">
+                        <div class="tab">
+                            <button class="button" v-on:click="switchTab('reg')">Registrarse</button>
+                            <button class="button" v-on:click="switchTab('login')">Iniciar Sesión</button>
+                        </div>
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                     </div>
 
                     <div class="modal-body">
-                        <fieldset>
-                            <legend><h3>Registrarse</h3></legend>
-                            <p>Nombre de usuario:</p>
-                            <input type="text" v-model="username">
-                            <p>{{username}}</p>
-                            <p>E-mail:</p>
-                            <input type="email" name="email">
-                            <p>Contraseña</p>
-                            <input type="password" name="password"><br/>
-                            <input type="button" value="Registrarse">
-                            <a href=""><p>Ya tengo una cuenta en Pokemarkt</p></a>
-                        </fieldset>
-
+                        <div id="registerTab" v-if="tab">
+                            <fieldset>
+                                <legend><h3>Registrarse</h3></legend>
+                                <p>Nombre de usuario:</p>
+                                <input type="text" v-model="username">
+                                <p>E-mail:</p>
+                                <input type="email" v-model="email">
+                                <p>Contraseña</p>
+                                <input type="password" v-model="password"><br/>
+                                <input type="button" value="Registrarse">
+                                <a href=""><p>Ya tengo una cuenta en Pokemarkt</p></a>
+                            </fieldset>
+                        </div>
+                        <div id="loginTab" v-else>
+                            <fieldset>
+                                <legend><h3>Iniciar sesión</h3></legend>
+                                <p>Nombre de usuario o email:</p>
+                                <input type="text" v-model="username">
+                                <p>Contraseña</p>
+                                <input type="password" v-model="password"><br/>
+                                <input type="button" v-on:click="validateData" value="Iniciar sesión">
+                            </fieldset>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -37,12 +51,19 @@ export default {
         return{
             username:"",
             email:"",
-            password:""
+            password:"",
+            tab:false
         }
     },    
     methods: {
         validateData() {
             
+        },
+        switchTab(t){
+            if(t=='reg')
+                this.tab = true
+            else
+                this.tab = false
         }
     },
     watch:{
